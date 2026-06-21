5106c23893bfb836de0c3a6b1cb60fb8c54e29ab
thorvald <thor.gudj@gmail.com>
2026-06-21 22:37:04 +0200

    map tags to common_tags

--- locals.tf
+++ locals.tf
@@ -1,7 +1,9 @@
 locals{
-    Environment = var.environment
-    Project     = "PlatformFoundation"
-    Owner       = var.owner
-    ManagedBy   = "Terraform"
-    CostCenter  = var.cost_center
+    common_tags = {
+        Environment = var.environment
+        Project     = "PlatformFoundation"
+        Owner       = var.owner
+        ManagedBy   = "Terraform"
+        CostCenter  = var.cost_center
+    }
 }
